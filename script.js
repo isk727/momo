2b29ce63be3e063f0736d63ee49394583a36e6b1
isk727 <isk727@gmail.com>
2024-01-26 22:09:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-//$(function(){$('<div id="start_bu"></div>tton').click(function(e){connect();$("#start").hide();});});$(window).on("beforeunload",function(e){disconnect();});
-  const video = document.getElementById('remote_video');
-  $("#sound").on('click', function () {
-    if(video.muted){
-      $("#speaker").attr('src', 'img/sound_on.png');
-      video.muted = false;
-    }else{
-      $("#speaker").attr('src', 'img/sound_off.png');
-      video.muted = true;
-    }
-  });
-  connect();
+const video = document.getElementById('remote_video');
+$("#sound").on('click', function () {
+  if(video.muted){
+    $("#speaker").attr('src', 'img/sound_on.png');
+    video.muted = false;
+  }else{
+    $("#speaker").attr('src', 'img/sound_off.png');
+    video.muted = true;
+  }
+});
+connect();
